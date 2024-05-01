3feaf6f7005b7278fba8ae64a3eb4f5d7c77593c
ZalohaD <zaloha.denys@gmail.com>
2024-05-01 23:25:44 +0300

    Fix testimonials.blade.php

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -33,7 +33,7 @@
                     <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
                     <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
                     <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-3.jpg" style="width: 50px; height: 50px;">
+                        <img class="img-fluid flex-shrink-0 rounded" src="{{url('img/testimonial-3.jpg')}}" style="width: 50px; height: 50px;">
                         <div class="ps-3">
                             <h5 class="mb-1">Client Name</h5>
                             <small>Profession</small>
@@ -44,7 +44,7 @@
                     <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
                     <p>Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>
                     <div class="d-flex align-items-center">
-                        <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-4.jpg" style="width: 50px; height: 50px;">
+                        <img class="img-fluid flex-shrink-0 rounded" src="{{url('img/testimonial-4.jpg')}}" style="width: 50px; height: 50px;">
                         <div class="ps-3">
                             <h5 class="mb-1">Client Name</h5>
                             <small>Profession</small>
